a44b714faa821d4aff93d3ade53303c74430f95f
white156 <white156@qq.com>
2022-01-07 00:18:27 +0800

    测试用:登录页面

---
+++ src/components/LogIn.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      user name: <input type="text" v-model="userForm.name" placeholder="user name"/>
+      <br/>
+      passWord : <input type="password" v-model="userForm.passWord" placeholder="password"/>
+      <br/>
+      <button @click="login">log in</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      userForm: {
+        name: '',
+        passWord: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$axios.post('/api/login', this.userForm).then((result) => {
+        if (result.data.code === 200) {
+          this.$router.replace({path: '/HelloWorld'})
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
